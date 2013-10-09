ba5164ab74c9a131380f87920ee00a5aacd49fa1
anderfernandes <handerson171@gmail.com>
2013-10-08 20:52:11 -0500

    Adding Speed Converter
    Adding Speed Converter program done on 10/8/2013.

---
+++ Speed Converter/Speed Converter/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Speed_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.presortListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.Location = new System.Drawing.Point(12, 12);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(260, 186);
+            this.outputListBox.TabIndex = 0;
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(17, 258);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(123, 35);
+            this.displayButton.TabIndex = 1;
+            this.displayButton.Text = "&Display Speeds";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(146, 258);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(126, 35);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "&Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // presortListBox
+            // 
+            this.presortListBox.FormattingEnabled = true;
+            this.presortListBox.Location = new System.Drawing.Point(17, 235);
+            this.presortListBox.Name = "presortListBox";
+            this.presortListBox.Size = new System.Drawing.Size(23, 17);
+            this.presortListBox.TabIndex = 3;
+            this.presortListBox.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Search:";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(64, 215);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(100, 20);
+            this.searchTextBox.TabIndex = 5;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(181, 213);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(91, 23);
+            this.searchButton.TabIndex = 6;
+            this.searchButton.Text = "&Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 303);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.presortListBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.outputListBox);
+            this.Name = "Form1";
+            this.Text = "Speed Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ListBox presortListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ Speed Converter/Speed Converter/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Speed_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            // Constants
+            const int START_SPEED = 60;
+            const int END_SPEED = 130;
+            const int INTERVAL = 10;
+            const double CONVERSION_FACTOR = 0.6214;
+            
+
+            // Variables
+            int kph;    // Kilometers per hour
+            double mph; // Miles per hour
+            int count = 0; // Counter
+
+            // Display the table of speeds
+            for (kph = START_SPEED; kph <= END_SPEED; kph += INTERVAL)
+            {
+                // Calculate miles per hour
+                mph = kph * CONVERSION_FACTOR;
+
+                // Display the conversion
+                presortListBox.Items.Add(kph + " KPH is the same as " + mph + " MPH");
+                count++;
+            }
+
+            // Show result in reverse order. 
+            for (count--; count >= 0; count--)
+            {
+                outputListBox.Items.Add(presortListBox.Items[count].ToString());
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string searchString;    // Variable that will hold the string to be found
+
+            searchString = searchTextBox.Text; // Assigining whatever is in the text box to the variable
+
+            if (searchString != string.Empty) // If the text box isn't empty
+            {
+                int index = outputListBox.FindString(searchString); // Find string on outputListBox
+
+                if (index != -1)
+                    outputListBox.SetSelected(index, true); // Selected string value
+                else
+                    MessageBox.Show("The search string did not find any results.");
+            }
+        }
+    }
+}
