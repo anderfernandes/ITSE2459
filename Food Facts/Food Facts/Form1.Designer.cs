19d87cf6505b5de38827e1b7ffbfd944f084528d
anderfernandes <handerson171@gmail.com>
2013-11-14 20:42:49 -0600

    Added tutorials 9-4, 9-5
    Added tutorials 9-4, 9-5

---
+++ Food Facts/Food Facts/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Food_Facts
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.bananaRadioButton = new System.Windows.Forms.RadioButton();
+            this.popcornRadioButton = new System.Windows.Forms.RadioButton();
+            this.muffinRadioButton = new System.Windows.Forms.RadioButton();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Food:";
+            // 
+            // bananaRadioButton
+            // 
+            this.bananaRadioButton.AutoSize = true;
+            this.bananaRadioButton.Location = new System.Drawing.Point(45, 82);
+            this.bananaRadioButton.Name = "bananaRadioButton";
+            this.bananaRadioButton.Size = new System.Drawing.Size(71, 17);
+            this.bananaRadioButton.TabIndex = 4;
+            this.bananaRadioButton.TabStop = true;
+            this.bananaRadioButton.Text = "1 Banana";
+            this.bananaRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // popcornRadioButton
+            // 
+            this.popcornRadioButton.AutoSize = true;
+            this.popcornRadioButton.Location = new System.Drawing.Point(45, 115);
+            this.popcornRadioButton.Name = "popcornRadioButton";
+            this.popcornRadioButton.Size = new System.Drawing.Size(106, 17);
+            this.popcornRadioButton.TabIndex = 5;
+            this.popcornRadioButton.TabStop = true;
+            this.popcornRadioButton.Text = "1 cup of popcorn";
+            this.popcornRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // muffinRadioButton
+            // 
+            this.muffinRadioButton.AutoSize = true;
+            this.muffinRadioButton.Location = new System.Drawing.Point(45, 149);
+            this.muffinRadioButton.Name = "muffinRadioButton";
+            this.muffinRadioButton.Size = new System.Drawing.Size(134, 17);
+            this.muffinRadioButton.TabIndex = 6;
+            this.muffinRadioButton.TabStop = true;
+            this.muffinRadioButton.Text = "1 large blueberry muffin";
+            this.muffinRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(45, 187);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(82, 36);
+            this.displayButton.TabIndex = 7;
+            this.displayButton.Text = "Display Food Facts";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(157, 187);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 36);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.muffinRadioButton);
+            this.Controls.Add(this.popcornRadioButton);
+            this.Controls.Add(this.bananaRadioButton);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Food Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton bananaRadioButton;
+        private System.Windows.Forms.RadioButton popcornRadioButton;
+        private System.Windows.Forms.RadioButton muffinRadioButton;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Multiform Practice/Multiform Practice/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Multiform_Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainForm = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.MainForm.Location = new System.Drawing.Point(54, 107);
+            this.MainForm.Name = "MainForm";
+            this.MainForm.Size = new System.Drawing.Size(75, 23);
+            this.MainForm.TabIndex = 0;
+            this.MainForm.Text = "Display Form";
+            this.MainForm.UseVisualStyleBackColor = true;
+            this.MainForm.Click += new System.EventHandler(this.MainForm_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.MainForm);
+            this.Name = "Form1";
+            this.Text = "Multiform Practice";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MainForm;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
